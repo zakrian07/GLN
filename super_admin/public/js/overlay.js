64adbde25d3d3049ba269df4e9e7e7084a497f46
usman <usmanhafeez147@gmail.com>
2016-12-08 12:49:44 -0800

    Updated version

--- super_admin/public/js/overlay.js
+++
@@ -1,9 +0,0 @@
-/* Open when someone clicks on the span element */
-function openOverLay() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeOverLay() {
-    document.getElementById("myNav").style.width = "0%";
-}
